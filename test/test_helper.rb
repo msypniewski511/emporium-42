652b0e1c3180b4e06d10019b944eeff279642833
msypniewski511 <msypniewski511@gmail.com>
2018-06-09 10:10:48 +0100

    set app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+##### minitest report #######
+require "minitest/reporters"
+Minitest::Reporters.use!
+##### end minitest report ###
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
