157930e072af4f129a2edfe22cc68f9af01a09a6
msypniewski511 <msypniewski511@gmail.com>
2018-06-17 10:07:40 +0100

    nastepna zmiana

--- db/migrate/20180610150327_nastepna_zmiana.rb
+++
@@ -1,6 +0,0 @@
-class NastepnaZmiana < ActiveRecord::Migration
-  def change
-    add_foreign_key :authors_books, :authors
-    add_foreign_key :authors_books, :books
-  end
-end
--- db/migrate/20180610155433_delete_forig.rb
+++
@@ -1,8 +0,0 @@
-class DeleteForig < ActiveRecord::Migration
-  # Usunolem niepotrzebne klucze obce
-  def change
-    remove_foreign_key :authors_books, :authors
-    remove_foreign_key :authors_books, :books
-    #rename_column :books, :admin_publisher_id, :publisher_id
-  end
-end
