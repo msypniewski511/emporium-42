9f8bb04e1efb5d0909e19d1b459ffef37763250b
msypniewski511 <msypniewski511@gmail.com>
2018-06-17 20:17:58 +0100

    g

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_controller.consider_all_requests_local = true
+
   config.action_view.debug_rjs                         = true
   config.action_controller.perform_caching             = false
   config.cache_classes = true
