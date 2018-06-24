e0ab9d4da3c484336c079779733427cac8d2e19e
msypniewski511 <msypniewski511@gmail.com>
2018-06-24 12:51:18 +0100

    Added books review

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,4 +1,5 @@
 class CatalogController < ApplicationController
+  PAGE_SIZE = 5
   before_filter :initialize_cart
 
   def main
@@ -11,8 +12,27 @@ class CatalogController < ApplicationController
     @books = Book.includes(:authors, :publisher).paginate(page: params[:page], per_page: 10).order("books.id desc")
   end
 
+
+
+  ##############################################################################################
   def show
+
+    @page = (params[:page]||0).to_i
     @book = Book.find(params[:id]) rescue nil
+
+    if session[:book_id] == params[:id]
+      if params[:page].to_i == 1
+        session[:page_session] += 1
+      else
+        session[:page_session] -= 1
+      end
+    else
+      session[:book_id] = params[:id]
+      session[:page_session] = 0
+    end
+
+    @page = (session[:page_session]>=0 ? session[:page_session] : 0)
+    session[:page_session] = @page
     #TODO implement get related books###########################################
     @related_books = Book.latest
     #############################################################################
@@ -23,55 +43,39 @@ class CatalogController < ApplicationController
     @rating = ForumPost.average_votes(@book.id)
 
 
-
+    ############################################################################
+    # Check amount of posts for particular book and if is empty create new one #
+    # for form                                                                 #
+    ############################################################################
     if ForumPost.where(book_id: @book.id).empty?
       @post = ForumPost.new(parent_id: 0, book_id: @book.id)
       @new_review = true
     else
+
+
       ##########################################################################
       #TODO pobranie elemtow rewiew dla publikacji##############################
       ##########################################################################
       @reviews = ForumPost.where(["book_id = ? and parent_id = ?", @book.id, 0])
+      @koniec = @reviews.size/PAGE_SIZE
 
-
-
-
-
-
-
-
-
-
-
-
-      #@posts = ForumPost.where(["book_id = ? and parent_id = ?", @book.id, 0]).paginate(page: params[:page], per_page: 5)
-      @posts = ForumPost.where(["book_id = ? and parent_id = ?", @book.id, 0]).page(params[:page] || 1).per_page(5)
-
-      #Client.limit(5).offset(30)
-
-
-
-
-
-
-
-
-
-
-
+      # Search all posts for particular book and divide to pagination
+      @posts = ForumPost.where(["book_id = ? and parent_id = ?", @book.id, 0]).
+      offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
+      #########################################################################
 
       @post = ForumPost.new(parent_id: 0, book_id: @book.id)
-      #@posts = ForumPost.where(book_id: @book.id)
       @new_review = false
     end
-
     respond_to do |format|
       format.html
       format.xml  { render :xml => @ics }
-      format.js
+      format.js { render 'show', :locals => {:page_id => session[:page_session]}}
     end
   end
 
+
+  ###########################################################################################
   def search
     @page_title = "Search for: #{params[:q]}" || "Search"
     @books_all = Book.includes(:authors, :publisher).where('lower(title) LIKE lower(?) OR lower(blurb) LIKE lower(?)', "%#{params[:q]}%", "%#{params[:q]}%")
